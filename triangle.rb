8f27e9adf892be7b2066b03feeb8b92f528c9ea9
Rodrigo Uri Pérez Millán <urilooco@MacBook-Pro-de-Rodrigo.local>
2021-12-09 20:31:18 -0600

    The Ruby Koans exercise

---
+++ triangle.rb
@@ -0,0 +1,48 @@
+# Triangle Project Code.
+
+# Triangle analyzes the lengths of the sides of a triangle
+# (represented by a, b and c) and returns the type of triangle.
+#
+# It returns:
+#   :equilateral  if all sides are equal
+#   :isosceles    if exactly 2 sides are equal
+#   :scalene      if no sides are equal
+#
+# The tests for this method can be found in
+#   about_triangle_project.rb
+# and
+#   about_triangle_project_2.rb
+#
+def triangle(a, b, c)
+  sorted_triangle_lengths = [a, b, c].sort
+  raise TriangleError if (sorted_triangle_lengths[0] + sorted_triangle_lengths[1] <= sorted_triangle_lengths[2])
+
+  raise TriangleError if (a <= 0 || b <= 0 || c <= 0)
+
+  # if (a == b) && (a == c) && (b == c)
+  #   :equilateral
+  # elsif (a == b) && ((a || b) != c)
+  #   :isosceles
+  # elsif (a == c) && ((a || c) != b)
+  #   :isosceles
+  # elsif (b == c) && ((b || c) != a)
+  #   :isosceles
+  # else
+  #   :scalene
+  # end
+
+  return :equilateral if a == b && a == c
+  return :isosceles if a == b || b == c || a == c
+  return :scalene if a != b && b != c && a != c
+
+  # sides = [a, b, c].uniq
+  # type = case sides.length
+  #   when 1 then :equilateral
+  #   when 2 then :isosceles
+  #   when 3 then :scalene
+  #   end
+end
+
+# Error class used in part 2.  No need to change this code.
+class TriangleError < StandardError
+end
